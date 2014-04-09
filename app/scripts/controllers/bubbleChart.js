c970445a5eca05f63d1e473ee956823537696f02
Kevin Soong <kjsery@gmail.com>
2014-04-09 15:13:26 -0400

    pathVizService: Moved data to model service.

--- app/scripts/controllers/bubbleChart.js
+++ app/scripts/controllers/bubbleChart.js
@@ -6,54 +6,9 @@ angular.module('a3App')
         width: 800,
         height: 1200
     };
-    $scope.courseData = [];
-    $scope.alumniData = [];
+    $scope.courseData = pathVizService.courseData;
+    $scope.alumniData = pathVizService.alumniData;
     $scope.selectedAlumni = [];
-    $scope.onCourseDataLoaded = function (data) {
-        angular.forEach(data, function (row, index) {
-            // Skip header
-            if (index === 0)
-                return;
-            $scope.courseData.push({
-                id: row[0],
-                number: row[1],
-                name: row[2],
-                coord: {x: row[0]/3 * 100, y: index%3 * 100},
-                popularity: row[3]
-            });
-        });
-        console.log($scope.courseData);
-    }
-    pathVizService.loadCourseData($scope.onCourseDataLoaded);
-
-    $scope.onAlumniDataLoaded = function (data) {
-        angular.forEach(data, function (row, index) {
-            // Skip header
-            if (index === 0)
-                return;
-            $scope.alumniData.push({
-                id: row[0],
-                coord: {x: -100, y: ((20)*row[0]-90)},
-                position: row[1],
-                name: row[2],
-                courses: [ row[3],row[4],row[5] ],
-                selected: false
-            });
-        });
-        angular.forEach($scope.alumniData, function (alumnus, index) {
-            alumnus.pathCoords = [];
-            alumnus.pathCoords.push(alumnus.coord.x + ' ' + alumnus.coord.y);
-            angular.forEach(alumnus.courses, function (courseId, index) {
-                var c = $scope.courseData[courseId].coord;
-                alumnus.pathCoords.push(c.x + ' ' + c.y);
-            });
-            alumnus.d = 'M ' + alumnus.pathCoords.join(' L ');
-            alumnus.d2 = alumnus.pathCoords.slice(1).join(' L ');
-        });
-
-        console.log($scope.alumniData);
-    }
-    pathVizService.loadAlumniData($scope.onAlumniDataLoaded);
 
     $scope.highlightPath = function (alumnus) {
         $scope.selectedAlumni.push(alumnus.id);
--- app/scripts/services/pathVizService.js
+++ app/scripts/services/pathVizService.js
@@ -3,25 +3,66 @@
 angular.module('a3App')
   .service('pathVizService', function (CsvReaderService) {
     var self = this;
-    self.rawData = {};
-    self.loadDataWithUrl = function(url, callback) {
-      CsvReaderService.read(
-            url,
-            function(d) {
-              console.log(d);
-              self.rawData = d;
-              callback(d);
-            }
-      );
-    }
+
+    self.courseData = [];
+    self.alumniData = [];
     self.loadData = function (callback) {
-      self.loadDataWithUrl('/images/anon689_2.csv', callback);  
+      CsvReaderService.read('/images/anon689_2.csv', callback);  
     }
     self.loadCourseData = function (callback) {
-      self.loadDataWithUrl('/images/course.csv', callback);
+      CsvReaderService.read('/images/course.csv', callback);
     }
     self.loadAlumniData = function (callback) {
-      self.loadDataWithUrl('/images/alumni.csv', callback);
+      CsvReaderService.read('/images/alumni.csv', callback);
     }
-    
+    self.onCourseDataLoaded = function (data) {
+        angular.forEach(data, function (row, index) {
+            // Skip header
+            if (index === 0)
+                return;
+            self.courseData.push({
+                id: row[0],
+                number: row[1],
+                name: row[2],
+                coord: {x: row[0]/3 * 100, y: index%3 * 100},
+                popularity: row[3]
+            });
+        });
+        console.log(self.courseData);
+    }
+    self.loadCourseData(self.onCourseDataLoaded);
+
+    self.onAlumniDataLoaded = function (data) {
+        angular.forEach(data, function (row, index) {
+            // Skip header
+            if (index === 0)
+                return;
+            self.alumniData.push({
+                id: row[0],
+                coord: {x: -100, y: ((20)*row[0]-90), originalY: ((20)*row[0]-90)},
+                position: row[1],
+                name: row[2],
+                courses: [ row[3],row[4],row[5] ],
+                hidden: false,
+                selected: false
+            });
+        });
+        angular.forEach(self.alumniData, function (alumnus, index) {
+            alumnus.pathCoords = [];
+            alumnus.pathCoords.push(alumnus.coord.x + ' ' + alumnus.coord.y);
+            angular.forEach(alumnus.courses, function (courseId, index) {
+                var c = self.courseData[courseId].coord;
+                alumnus.pathCoords.push(c.x + ' ' + c.y);
+            });
+            alumnus.d = 'M ' + alumnus.pathCoords.join(' L ');
+            alumnus.d2 = alumnus.pathCoords.slice(1).join(' L ');
+        });
+
+        console.log(self.alumniData);
+    }
+    self.loadAlumniData(self.onAlumniDataLoaded);
+
+
+
+
   });
