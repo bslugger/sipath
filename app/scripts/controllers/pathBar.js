8b792b2724c5dc5bd05e2826a55a9939e2354276
Kevin Soong <kjsery@gmail.com>
2014-04-24 01:50:24 -0400

    PathBar: show on default

--- app/scripts/controllers/pathBar.js
+++ app/scripts/controllers/pathBar.js
@@ -4,7 +4,7 @@ angular.module('a3App')
   .controller('PathBarCtrl', function ($scope, pathVizService) {
     $scope.selectedBgName = pathVizService.selectedBgName;
     $scope.selectedPosName = pathVizService.selectedPosName;
-    $scope.expanded = false;
+    $scope.expanded = true;
     $scope.toggle = function () {
         pathVizService.filterAlumni();
         $scope.expanded = !$scope.expanded;
